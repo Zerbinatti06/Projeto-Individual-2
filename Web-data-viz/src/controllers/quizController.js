81c9c567ff605faa569dba48ac290d7c30c0e4cd
Zerbinatti06 <desouzag477@gmail.com>
2026-07-16 19:23:46 -0300

    fix: Improving getQuiz route response

--- Web-data-viz/src/controllers/quizController.js
+++ Web-data-viz/src/controllers/quizController.js
@@ -3,10 +3,62 @@ var quizModel = require("../models/quizModel");
 function getQuiz(req, res) {
     var id = req.params.id;
 
+    let response;
+    let quizInfo;
+    let questions = [];
+    let answers = [];
+
     quizModel.getQuiz(id)
-    .then(response => {
-        res.json(response).status(200);
-        console.log("quiz enviado com sucesso" + response);
+    .then(quiz => {
+        
+        quizInfo = {
+            id:quiz[0].id,
+            userId:quiz[0].userId,
+            theme:quiz[0].theme,
+            title:quiz[0].title,
+            description:quiz[0].description,
+            createdAt:quiz[0].createdAt,
+        }
+
+        let ids = [];
+
+        for(let i = 0; i < quiz.length; i++){
+                let question = {
+                        id:quiz[i].questionId,
+                        quizId:quiz[i].quizId,
+                        question:quiz[i].question,
+                        answers: []
+                    }
+
+                if(!ids.includes(question.id)){
+                    ids.push(question.id);
+                    questions.push(question);
+                }
+
+                answers.push(
+                    {
+                        id:quiz[i].answerId,
+                        questionId:quiz[i].questionId,
+                        text:quiz[i].text,
+                        isRight:quiz[i].isRight
+                    }
+                )
+        }
+
+        for(let i = 0; i < answers.length; i++) {
+            let id = answers[i].questionId;
+
+            for(let j = 0; j < questions.length; j++){
+                    if(questions[j].id == id){
+                        questions[j].answers.push(answers[i]);
+                    }
+            }
+        }
+
+        response = quizInfo;
+        response.questions = questions;
+
+        res.send(response);
     })
     .catch(err => {
         console.log(err)
--- Web-data-viz/src/models/quizModel.js
+++ Web-data-viz/src/models/quizModel.js
@@ -2,9 +2,31 @@ var database = require("../database/config")
 
 function getQuiz(id) {
     var sql = `
-        SELECT * FROM quiz q
-        JOIN question qu on q.id = qu.quizId
-        JOIN answer a on qu.id = a.questionId
+        SELECT
+            q.id,
+            q.userId,
+            q.theme,
+            q.title,
+            q.description,
+            q.createdAt,
+
+            qu.id AS questionId,
+            qu.quizId,
+            qu.question,
+
+            a.id AS answerId,
+            a.questionId,
+            a.text,
+            a.isRight
+
+        FROM Tamarineira.quiz q
+
+        JOIN Tamarineira.question qu
+            ON q.id = qu.quizId
+
+        JOIN Tamarineira.answer a
+            ON qu.id = a.questionId
+
         WHERE q.id = ${id};
     `;
 
